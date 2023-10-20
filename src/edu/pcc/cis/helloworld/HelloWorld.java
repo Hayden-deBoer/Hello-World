7e7ab681053de2a5b08f9d59fc2ffdf9abd8436d
Gray (Hayden) deBoer <hayden.deboer@pcc.edu>
2023-10-20 16:59:29 -0700

    Added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,10 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Created by Gray deBoer on 2023.10.20
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
