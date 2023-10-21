dc33bf83926e7ae1951969f21da492e14be5878a
Gray (Hayden) deBoer <hayden.deboer@pcc.edu>
2023-10-20 17:01:42 -0700

    Print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,10 +1,13 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * Created by Gray deBoer on 2023.10.20
  */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
